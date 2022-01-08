0300a2a6d22cc5ae576ed8a808f11a25020c4b6b
Nile Okomo <okomonile3@gmail.com>
2022-01-08 22:41:35 +0300

    Add prototype for bubble_sort function

--- sort.h
+++ sort.h
@@ -4,9 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
 /*Doubly linked list*/
 
 /**
@@ -22,5 +19,11 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/*Function prototypes*/
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
