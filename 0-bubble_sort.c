ce94943e08337118df4f57812bcefd346caab4a8
Nile Okomo <okomonile3@gmail.com>
2022-01-09 11:36:12 +0300

    Break loop if list is sorted

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,8 +3,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t index;
+	int swapped;
 
-	while (*array)
+	swapped = check_sort(array, size);
+	while (swapped == 0)
 	{
 		for (index = 1; index < size; ++index)
 		{
@@ -15,7 +17,24 @@ void bubble_sort(int *array, size_t size)
 				array[index - 1] ^= array[index];
 				print_array(array, size);
 			}
+			swapped = check_sort(array, size);
 		}
-
 	}
+}
+
+int check_sort(int *array, size_t size)
+{
+	size_t index;
+	int boolean = 0;
+
+	for (index = 1; index < size; ++index)
+	{
+		if (array[index - 1] > array[index])
+		{
+			boolean = 0;
+			break;
+		}
+		boolean = 1;
+	}
+	return (boolean);
 }
