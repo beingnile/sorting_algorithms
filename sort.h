b1acdfa4f9f3e2cbdc0b682f3fc2bce14eeb5654
Nile Okomo <okomonile3@gmail.com>
2022-01-11 19:35:15 +0300

    Add prototype for quick sort implementation

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ int check_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *left, listint_t *right, listint_t **head);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
