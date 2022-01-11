f84003e6828137ef634f58e558ba802643acb2c8
Nile Okomo <okomonile3@gmail.com>
2022-01-11 19:12:43 +0300

    Add prototype for selection sort

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void bubble_sort(int *array, size_t size);
 int check_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *left, listint_t *right, listint_t **head);
+void selection_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
