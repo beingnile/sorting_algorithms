81de65a9d21ab6abfac4fa60b142ddbd98e3841c
Nile Okomo <okomonile3@gmail.com>
2022-01-11 17:33:03 +0300

    Implement insertion sort algorithm on doubly linked lists

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -20,7 +20,7 @@ void swap(listint_t *left, listint_t *right, listint_t **head)
 	right->next = left;
 	if (right->prev == NULL)
 		*head = right;
-	if (left->next != NULL)
+	if (left->next)
 		left->next->prev = left;
 	print_list(*head);
 }
@@ -32,4 +32,23 @@ void swap(listint_t *left, listint_t *right, listint_t **head)
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *node, *nextnode, *prevnode, *temp;
+
+	if (list == NULL)
+		return;
+
+	node = nextnode = *list;
+	while (node)
+	{
+		while (node->prev)
+		{
+			prevnode = node->prev;
+			temp = prevnode;
+			if (prevnode->n > node->n)
+				swap(prevnode, node, list);
+			node = temp;
+		}
+		node = nextnode->next;
+		nextnode = node;
+	}
 }
