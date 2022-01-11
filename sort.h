9d96111ffdb1a5b05c0cc90c130c7f79df055b24
Nile Okomo <okomonile3@gmail.com>
2022-01-11 17:12:22 +0300

    Update header file with new function

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 int check_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap(listint_t *left, listint_t *right, listint_t **head);
 
 #endif /*SORT_H*/
